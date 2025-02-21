d077cfb44a5cabc968dd3b4136eb58bed25e5072
Tamiiv <tamiis1996.tv@gmail.com>
2025-02-21 14:19:48 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import LayoutHome from './components/LayoutHome'
+
+
+function App() {
+
+  return (
+    <div>
+      <LayoutHome></LayoutHome>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LayoutHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import css from '../styles/components/layout.module.scss'
+import Header from './Header.jsx'
+
+export default function LayoutHome() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
