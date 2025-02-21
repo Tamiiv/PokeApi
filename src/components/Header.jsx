69a7aaf33f56334f82c7d179804abc35486fade4
Tamiiv <tamiis1996.tv@gmail.com>
2025-02-21 17:48:04 -0300

    Desarrollo Layout y Card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+//import React from 'react'
 import css from '../styles/components/header.module.scss'
 import logo from '../assets/pokemon.png'
 
--- src/components/LayoutHome.jsx
+++ src/components/LayoutHome.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import css from '../styles/components/layout.module.scss'
 import Header from './Header.jsx'
+import Card from './Card.jsx'
+import { URL_POKEMON } from '../api/apiRest.js'
+import axios from 'axios'
 
 export default function LayoutHome() {
+  const [arrayPokemon,setArrayPokemon] = useState([])
+
+  useEffect(() => {
+    const api = async () => {
+      const apiPoke = await axios.get(`${URL_POKEMON}`)
+      console.log("Array")
+      console.log(apiPoke.data.results)
+      setArrayPokemon(apiPoke.data.results)
+    }
+
+    api()
+  }, [])
+
   return (
-    <div>
+    <div className={css.layout}>
       <Header/>
+
+      <div>
+        {
+          arrayPokemon.map((card, index) => {
+            console.log("valor card")
+            console.log(card)
+            return <Card key={index} card={card} />
+          })
+        }
+      </div>
     </div>
   )
 }
